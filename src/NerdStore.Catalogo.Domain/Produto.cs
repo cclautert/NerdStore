c9274eb3b4f734f9571dc417b0c25950c6940587
cristiano.c.lautert@gmail.com <cristiano.c.lautert@gmail.com>
2021-06-07 19:05:24 -0300

    [FIRST-COMMIT] Criação do Projeto NerdStore da Formação Arquiteto de Software.

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,66 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        private string _Nome;
+        public string Nome {
+            get { return _Nome; }
+            private set { _Nome = value; }
+        }
+
+        private string _Descricao;
+
+        public string Descricao {
+            get { return _Descricao; }
+            private set { _Descricao = value; }
+        }
+
+        private bool _Ativo;
+        public bool Ativo {
+            get { return _Ativo; }
+            private set { _Ativo = value; }
+        }
+
+        private decimal _Valor;
+        public decimal Valor {
+            get { return _Valor; }
+            private set { _Valor = value; }
+        }
+
+        private DateTime _DataCadastro;
+        public DateTime DataCadastro {
+            get { return _DataCadastro; }
+            private set { _DataCadastro = value; }
+        }
+
+        private string _Imagem;
+        public string Imagem {
+            get { return _Imagem; }
+            private set { _Imagem = value; }
+        }
+
+        private int _QuantidadeEstoque;
+        public int QuantidadeEstoque {
+            get { return _QuantidadeEstoque; }
+            private set { _QuantidadeEstoque = value; }
+        }
+
+        private Categoria _Categoria;
+        public Categoria Categoria {
+            get { return _Categoria; }
+            private set { _Categoria = value; }
+        }
+
+    }
+
+    public class Categoria : Entity, IAggregateRoot
+    {
+    }
+}
---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        private Guid _Id;
+        public Guid Id {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+
+        protected Entity()
+        {
+            _Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return _Id.Equals(compareTo._Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+            if (ReferenceEquals(a, null) | ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + _Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " [Id=" + _Id + "]";
+        }
+    }
+}
